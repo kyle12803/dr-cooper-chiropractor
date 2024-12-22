e86a73789cbbb9a7d5d3f254958936ae53b9b60f
Kyle C <kyle.4334@yahoo.com>
2024-12-22 00:12:06 -0500

    debug env

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -24,6 +24,7 @@ const Contact = () => {
 		setIsSubmitting(true);
 
 		try {
+			console.log(process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID);
 			const response = await emailjs.send(
 				process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
 				process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
